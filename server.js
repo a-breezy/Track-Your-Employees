1dcfcccd0df13abfdc486188f2855b29d2ad8d49
a.breezy <ambrose.wilkins@gmail.com>
2021-12-25 12:11:42 -0500

    create server.js file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const db = require("./db/connnection");
+const apiRoutes = require("./routes/apiRoutes");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// default response for any request -- Not Found
+app.use((req, res) => {
+	res.status(404).end();
+});
+
+db.connect((err) => {
+	if (err) throw err;
+	console.log("Database Connected");
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
+});
