39c40240c2cfc89d8e11515ff8adf788ec783b96
a.breezy <ambrose.wilkins@gmail.com>
2021-12-26 17:50:18 -0500

    add post routes to department, employee, and role

--- routes/apiRoutes/department.js
+++ routes/apiRoutes/department.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection");
+const inputCheck = require("../../utils/inputCheck");
 
 // endpoint to show data for all departments
 router.get("/depts", (req, res) => {
@@ -13,6 +14,30 @@ router.get("/depts", (req, res) => {
 		res.json({
 			message: "success",
 			data: rows,
+		});
+	});
+});
+
+// endpoint to add data for all departments
+router.post("/depts", ({ body }, res) => {
+	const errors = inputCheck(body, "name");
+	if (errors) {
+		res.status(400).json({ error: errors });
+		return;
+	}
+
+	const sql = `INSERT INTO department (name)
+                VALUES (?)`;
+	const params = [body.name];
+
+	db.query(sql, params, (err, result) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+		res.json({
+			message: "success",
+			data: body,
 		});
 	});
 });
--- routes/apiRoutes/employee.js
+++ routes/apiRoutes/employee.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection");
+const inputCheck = require("../../utils/inputCheck");
 
-// endpoint to show data for all departments
+// endpoint to show data for all employees
 router.get("/employees", (req, res) => {
 	const sql = `SELECT * FROM employee`;
 	db.query(sql, (err, rows) => {
@@ -13,6 +14,41 @@ router.get("/employees", (req, res) => {
 		res.json({
 			message: "success",
 			data: rows,
+		});
+	});
+});
+
+// endpoint to add data for all employees
+router.post("/employees", ({ body }, res) => {
+	const errors = inputCheck(
+		body,
+		"first_name",
+		"last_name",
+		"role_id",
+		"manager_id"
+	);
+	if (errors) {
+		res.status(400).json({ error: errors });
+		return;
+	}
+
+	const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                VALUES (?,?,?,?)`;
+	const params = [
+		body.first_name,
+		body.last_name,
+		body.role_id,
+		body.manager_id,
+	];
+
+	db.query(sql, params, (err, result) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+		res.json({
+			message: "success",
+			data: body,
 		});
 	});
 });
